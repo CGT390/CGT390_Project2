8c7b5e21437fe5c0b7bc66c0153e0ee3b5eb4988
Gillian Hanley <gghanley04@gmail.com>
2026-03-10 16:31:14 -0400

    adding app

---
+++ src/app/navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="navbar-title">CGT 390 Project 2</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
